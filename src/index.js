41343403ccc16ecc5d31a1d6aa84aee0e9db422d
Laura Hope <ssteamlaura@gmail.com>
2023-04-18 10:19:38 -0700

    Refactor code to include promises and static methods.

--- src/index.js
+++ src/index.js
@@ -1,83 +1,68 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather.service';
 
 // Business Logic
 
-// this is the code that handles creating our API call - making a request, sending it, and handling the response
 function getWeather(city) {
-  let request = new XMLHttpRequest(); 
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-  // const url2 = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${process.env.API_KEY_2}`;
-
-  request.addEventListener("loadend", function() { 
-    const response = JSON.parse(this.responseText); 
-    // console.log(response);
-    if (this.status === 200) {
-      printElements(response, city); 
-    } else {
-      printError(this, response, city);
-    }
+  let promise = WeatherService.getWeather(city);
+  promise.then(function(weatherDataArray) {
+    printElements(weatherDataArray);
+  }, function(errorArray) {
+    printError(errorArray);
   });
-
-  request.open("GET", url, true);
-  request.send();
 }
 
 function getWeatherByZip(zipCode) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${zipCode},us&appid=${process.env.API_KEY_2}`;
-
-  request.addEventListener("loadend", function() { 
-    const response = JSON.parse(this.responseText); 
-    if (this.status === 200) {
-      printElements(response, zipCode); 
-    } else {
-      printError(this, response, zipCode);
-    }
+  let promise = WeatherService.getWeatherByZip(zipCode);
+  promise.then(function(weatherDataArray) {
+    printElements(weatherDataArray);
+  }, function(errorArray) {
+    printError(errorArray);
   });
-
-  request.open("GET", url, true);
-  request.send();
 }
 
-function getWeatherBy5Day(lat, lon) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY_3}`;
+// function getWeatherBy5Day(lat, lon) {
+//   let request = new XMLHttpRequest();
+//   const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`;
 
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, lat, lon);
-    } else {
-      printError(this, response, lat, lon); 
-    }
-  });
+//   request.addEventListener("loadend", function() {
+//     const response = JSON.parse(this.responseText);
+//     if (this.status === 200) {
+//       printElements(response, lat, lon);
+//     } else {
+//       printError(this, response, lat, lon); 
+//     }
+//   });
 
-  request.open("GET", url, true);
-  // request.send();
-}
+//   request.open("GET", url, true);
+//   request.send();
+// }
 
 // UI Logic
 
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+function printElements(data) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${data[1]} is ${data[0].main.humidity}%.
+  The temperature in Farenheit is ${Number(((data[0].main.temp - 273.15)* 9/5 +32).toFixed(1))} degrees.`;
 }
 
-function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
-  The temperature in Farenheit is ${Number(((apiResponse.main.temp - 273.15)* 9/5 + 32).toFixed(1))} degrees. The wind speed is ${apiResponse.wind.speed} mph. Currently: ${apiResponse.weather[0].description}.`;
+function printError(error) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
   const city = document.querySelector('#location').value;
   const zipCode = document.querySelector('#location').value; 
-  const {Latitude: lat, Longitude: lon} = document.querySelector('#location').value; 
+  // const latLonObj = document.querySelector('#location').value; 
+  // const latLonArray = latLonObj.split(",");
+  // const lat = parseFloat(latLonArray[0]);
+  // const lon = parseFloat(latLonArray[1]);
   document.querySelector('#location').value = null;
   getWeather(city);
   getWeatherByZip(zipCode); 
-  getWeatherBy5Day(lat, lon); 
+  // getWeatherBy5Day(lat, lon); 
 }
 
 window.addEventListener("load", function() {
